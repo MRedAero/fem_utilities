4866f9dc7c9db7096af1db7f7fe74eb3b28b3e46
mredmond <michael.j.redmond@gmail.com>
2015-04-06 20:04:37 -0500

    1.  simplified bdf_reader

--- nastran/bdf/reader/bdf_reader.py
+++ nastran/bdf/reader/bdf_reader.py
@@ -84,25 +84,13 @@ class BDFReader(object):
 
         bdffile.close()
 
-        #cdef int i, j, continuations, line_size, goto
-
         line_size = len(lines)
-
-        continuations = 0
-
-        #goto = 0
 
         self._level += 1
 
-        # goto's are used so that xrange can be used instead of while statement; must faster
+        iterator = iter(xrange(line_size))
 
-        for i in xrange(line_size):
-
-            #print i
-
-            if continuations:
-                continuations -= 1
-                continue
+        for i in iterator:
 
             line = remove_comments(lines[i])
 
@@ -113,42 +101,58 @@ class BDFReader(object):
 
             if line[0:7] == 'INCLUDE':
 
-                goto = 1
-
                 include_line = line
 
-                continuations = 0
-                for j in xrange(i+1, line_size):
-                    if include_line.count("'") == 2:
-                        break
+                if include_line.count("'") != 2:
+                    while True:
+                        try:
+                            j = iterator.next()
+                        except StopIteration:
+                            break
 
-                    continuations += 1
-                    include_line += remove_comments(lines[j])
+                        include_line += remove_comments(lines[j])
 
-            if goto == 1:  # can this be merged under the previous if statement?
-                #print 'goto = 1'
-                # noinspection PyUnboundLocalVariable
+                        if include_line.count("'") == 2:
+                            break
+
                 include_file = include_line.split("'")[1]
                 self._read_bdf(include_file)
                 continue
 
-            if goto == 2:
-                #print 'goto = 2'
-                card = line[0:8].strip()
+            card = line[0:8].strip()
 
-                if r'*' in card:
-                    field_width = 16
-                else:
-                    field_width = 8
+            if r'*' in card:
+                field_width = 16
+            else:
+                field_width = 8
 
-                card = card.replace(r'*', '')
+            card = card.replace(r'*', '')
 
-                #print card
+            if card in self._card_keys:
+                card_line = '%-64s' % line[8:72]
 
-                if card in self._card_keys:
-                    card_line = '%-64s' % line[8:72]
+                bdf_line = i
 
-                    bdf_line = i
+                # noinspection PyBroadException
+                try:
+                    cont = line[72:81].strip()  # should it be 72:80?  I had a problem with this before
+                except Exception:
+                    cont = ''
+
+                j = i + 1
+
+                while True:
+
+                    line = remove_comments(lines[j])
+
+                    if line[0:8].strip() == cont:
+                        card_line += '%-64s' % line[8:72]
+                        try:
+                            j = iterator.next()
+                        except StopIteration:
+                            break
+                    else:
+                        break
 
                     # noinspection PyBroadException
                     try:
@@ -156,28 +160,6 @@ class BDFReader(object):
                     except Exception:
                         cont = ''
 
-                    goto = 3
-
-                    continuations = 0
-                    for j in xrange(i+1, line_size):
-                        line = remove_comments(lines[j])
-
-                        if line[0:8].strip() == cont:
-                            card_line += '%-64s' % line[8:72]
-                            continuations += 1
-                        else:
-                            break
-
-                        # noinspection PyBroadException
-                        try:
-                            cont = line[72:81].strip()  # should it be 72:80?  I had a problem with this before
-                        except Exception:
-                            cont = ''
-                else:
-                    logging.info("Card %s not supported." % card)
-
-            if goto == 3:
-                #print 'goto = 3'
                 if ',' in card_line:
                     data = map(convert_field, parse_string(card_line, ','))
                 else:
@@ -194,7 +176,8 @@ class BDFReader(object):
                     print 'BDF %s: line %d: field_width = %d\n%s' % (filename, bdf_line+1, field_width, card_line)
                     raise e
 
-                #goto = 0
+            else:
+                logging.info("Card %s not supported." % card)
 
         self._level -= 1
 
